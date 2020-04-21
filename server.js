b670ae99940aca96814826b6af46d243bb555139
megarbry <megarbry@gmail.com>
2020-04-21 09:54:35 -0600

    Added User model, added checks to user registration, and added password encryption

--- server.js
+++ server.js
@@ -7,9 +7,18 @@ const app = express();
 //Connect DB
 connectDB();
 
+//INit middleware
+app.use(express.json({extended: false}));
+
 app.get('/', (req, res) => {
     res.send('API running');
 })
+
+//Define routes
+app.use('/api/users', require('./routes/api/users'));
+app.use('/api/auth', require('./routes/api/auth'));
+app.use('/api/profile', require('./routes/api/profile'));
+app.use('/api/posts', require('./routes/api/post'));
 
 const PORT = process.env.PORT || 5000;
 
