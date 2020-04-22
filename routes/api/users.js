547fa48d211ce65ef0175ce44d085e3418de9036
megarbry <megarbry@gmail.com>
2020-04-22 11:07:34 -0600

    Created Profile model

--- routes/api/users.js
+++ routes/api/users.js
@@ -2,6 +2,8 @@ const express = require('express');
 const router = express.Router();
 const gravatar = require('gravatar');
 const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const config = require('config');
 const { check, validationResult } = require('express-validator');
 
 const User = require('../../models/User');
@@ -47,10 +49,20 @@ router.post('/', [
 
         await user.save();
 
-        //Return JWT
+        const payload = {
+            user: {
+               id: user.id
+            }
+        }
 
-        res.send('User created');
-
+        jwt.sign(payload, config.get('jwtSecret'),{
+            expiresIn: 3600
+        }, 
+            (err, token) => {
+                if(err) throw err;
+                res.json({ token });
+            }
+        )
     } catch(err) {
         console.error(err.message);
         res.status(500).send('Server error');
