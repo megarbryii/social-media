2614b5ecbb9b4249f5f3183431b6c908a7dbd8ba
megarbry <megarbry@gmail.com>
2020-05-01 10:25:22 -0600

    Started working on Dashboard

--- client/src/actions/auth.js
+++ client/src/actions/auth.js
@@ -8,7 +8,8 @@ import {
     AUTH_ERROR,
     LOGIN_SUCESS,
     LOGIN_FAIL,
-    LOGOUT
+    LOGOUT,
+    CLEAR_PROFILE
 } from './types';
 
 //Load user
@@ -97,7 +98,7 @@ export const login = (email, password) => async dispatch => {
 
 //Logout / Clear profile
 export const logout = () => async dispatch => {
-    dispatch({
-        type: LOGOUT
-    });
+    dispatch({type: CLEAR_PROFILE});
+    dispatch({type: LOGOUT});
+    
 }
--- client/src/components/dashboard/Dashboard.js
+++ client/src/components/dashboard/Dashboard.js
@@ -1,19 +1,30 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, Fragment } from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
+import { Link } from 'react-router-dom'
 
 import { getCurrentProfile } from '../../actions/profile';
 
-const Dashboard = ({ getCurrentProfile, auth, profile }) => {
+import Spinner from '../layouts/Spinner';
+
+const Dashboard = ({ getCurrentProfile, auth: { user }, profile: { profile, loading } }) => {
     useEffect(() => {
         getCurrentProfile();
     }, []);
 
-    return (
-        <div>
-            Dashboard
-        </div>
-    )
+    return loading && profile === null ? <Spinner /> : <Fragment>
+       <h1 className="large text-primary">Dashboard</h1>
+       <p className="lead">
+           <i className="fas fa-user"/> Welcome { user && user.name }
+       </p>
+       { profile !== null ? <Fragment>has</Fragment> : 
+       <Fragment>
+          <p>You do not set up a profile, please add some info</p> 
+          <Link to='/create-profile' className='btn btn-primary my-1'>
+            Create profile
+          </Link>
+        </Fragment>}
+    </Fragment>
 }
 
 Dashboard.propTypes = {
