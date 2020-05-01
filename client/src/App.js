71e13b7e6f70d100820e473933a14a5d57e06b14
megarbry <megarbry@gmail.com>
2020-05-01 13:00:17 -0600

    Made Create Profile component

--- client/src/App.js
+++ client/src/App.js
@@ -17,6 +17,7 @@ import Register from './components/auth/Register';
 import Alert from './components/layouts/Alert';
 import Dashboard from './components/dashboard/Dashboard';
 import PrivateRoute from './components/routing/PrivateRoute';
+import CreateProfile from './components/profile-form/CreateProfile';
 
 if(localStorage.token) {
   setAuthToken(localStorage.token);
@@ -40,6 +41,7 @@ const App = () => {
               <Route exact path='/register' component={Register} />
               <Route exact path='/login' component={Login} />
               <PrivateRoute exact path='/dashboard' component={Dashboard} />
+              <PrivateRoute exact path='/create-profile' component={CreateProfile} />
             </Switch>
           </section>
         </Fragment>
