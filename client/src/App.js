9d35bc51eb64e6e488d218dc9ba747e6d7532a03
megarbry <megarbry@gmail.com>
2020-05-01 09:04:24 -0600

    Created protected route for dashboard

--- client/src/App.js
+++ client/src/App.js
@@ -15,6 +15,8 @@ import Landing from './components/layouts/Landing';
 import Login from './components/auth/Login';
 import Register from './components/auth/Register';
 import Alert from './components/layouts/Alert';
+import Dashboard from './components/dashboard/Dashboard';
+import PrivateRoute from './components/routing/PrivateRoute';
 
 if(localStorage.token) {
   setAuthToken(localStorage.token);
@@ -37,6 +39,7 @@ const App = () => {
             <Switch>
               <Route exact path='/register' component={Register} />
               <Route exact path='/login' component={Login} />
+              <PrivateRoute exact path='/dashboard' component={Dashboard} />
             </Switch>
           </section>
         </Fragment>
