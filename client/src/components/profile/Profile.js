c594f91d723547af6a68aaa994748b57e6089a00
megarbry <megarbry@gmail.com>
2020-05-06 09:42:18 -0600

    Attempted Profile Top

--- client/src/components/profile/Profile.js
+++ client/src/components/profile/Profile.js
@@ -6,11 +6,12 @@ import { Link } from 'react-router-dom';
 import { getProfileById } from '../../actions/profile';
 
 import Spinner from '../layouts/Spinner';
+import ProfileTop from './ProfileTop';
 
 const Profile = ({ profile:  {profile, loading }, auth, match, getProfileById }) => {
     useEffect(() => {
         getProfileById(match.params.id);
-    }, [getProfileById])
+    }, [getProfileById, match.params.id])
 
     return (
         <Fragment>
@@ -18,6 +19,9 @@ const Profile = ({ profile:  {profile, loading }, auth, match, getProfileById })
                <Link to='/profiles' className='btn btn-light'>Back to Profiles</Link>
                {auth.isAuthenticated && auth.loading === false && auth.user._id === profile.user._id && (<Link to='/edit-profile' className='btn btn-dark'>Edit Profile</Link>)}
             </Fragment>}
+            <div className="profile-grid my-1">
+                <ProfileTop profile={profile} />
+            </div>
         </Fragment>
     )
 }
